1ac4ff7a8099afc428788a4a583b55445d9b06d2
Raul Agreda <100451269@alumnos.uc3m.es>
2024-03-07 12:21:21 +0100

    angle_test

--- motor_test.py
+++ motor_test.py
@@ -54,7 +54,7 @@ def set_servomotor_angle(servomotor_object, angle):
     ratio = (end - start) / 180
     angle_as_percent = angle * ratio
     # angle_as_percent = angle / 180 + 2.5
-    servomotor_object.ChangeDutyCycle(angle_as_percent + start)
+    servomotor_object.ChangeDutyCycle(angle_as_percent)
     print("New angle set successfully!")
 
 def servomotor_thread():
@@ -67,9 +67,9 @@ def servomotor_thread():
         target_angle = None
         try:
             target_angle = float(input("Enter the angle: "))
-            # if target_angle < 0 or target_angle > 180:
-            #     print("Invalid angle")
-            #     continue
+            if target_angle < 0 or target_angle > 180:
+                print("Invalid angle")
+                continue
         except:
             print("Invalid input")
             continue
